658cc40bcfab56d8e36acaec35fdf883d97217b7
jasmineChamCham <jasminebkdn@gmail.com>
2026-03-12 11:09:48 +0700

    chore: import modules into app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { PassportModule } from '@nestjs/passport';
 import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
+import { UserModule } from './modules/users';
+import { FavouriteIdeasModule } from './modules/favourite-ideas/favourite-ideas.module';
+import { GeminiModule } from './modules/gemini/gemini.module';
+import { MentorsModule } from './modules/mentors/mentors.module';
+import { SourcesModule } from './modules/sources/sources.module';
+import { SourceIdeasModule } from './modules/source-ideas/source-ideas.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), PassportModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    PassportModule,
+    UserModule,
+    FavouriteIdeasModule,
+    GeminiModule,
+    MentorsModule,
+    SourcesModule,
+    SourceIdeasModule,
+  ],
   controllers: [],
   providers: [],
 })
