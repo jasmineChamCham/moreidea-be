c5b4787f4ddccab08564f359e2411700a8908517
jasmineChamCham <jasminebkdn@gmail.com>
2026-04-19 14:01:02 +0700

    fix: get 25 relevant anchors

--- src/modules/anchors/application/searchRelatedAnchors/searchRelatedAnchors.handler.ts
+++ src/modules/anchors/application/searchRelatedAnchors/searchRelatedAnchors.handler.ts
@@ -1,21 +1,34 @@
 import { IQueryHandler, QueryHandler } from '@nestjs/cqrs';
-import { SearchRelatedAnchorsQuery, SearchRelatedAnchorsQueryResponse } from './searchRelatedAnchors.query';
+import {
+  SearchRelatedAnchorsQuery,
+  SearchRelatedAnchorsQueryResponse,
+} from './searchRelatedAnchors.query';
 import { QdrantService } from 'src/modules/qdrant/qdrant.service';
 import { EmbeddingsService } from 'src/modules/embeddings/embeddings.service';
 
 @QueryHandler(SearchRelatedAnchorsQuery)
-export class SearchRelatedAnchorsHandler implements IQueryHandler<SearchRelatedAnchorsQuery, SearchRelatedAnchorsQueryResponse> {
+export class SearchRelatedAnchorsHandler implements IQueryHandler<
+  SearchRelatedAnchorsQuery,
+  SearchRelatedAnchorsQueryResponse
+> {
   constructor(
     private readonly qdrantService: QdrantService,
     private readonly embeddingsService: EmbeddingsService,
-  ) { }
+  ) {}
 
-  async execute(query: SearchRelatedAnchorsQuery): Promise<SearchRelatedAnchorsQueryResponse> {
+  async execute(
+    query: SearchRelatedAnchorsQuery,
+  ): Promise<SearchRelatedAnchorsQueryResponse> {
     // Get embedding for the search text using embeddings service
-    const queryEmbedding = await this.embeddingsService.generateEmbedding(query.searchText);
+    const queryEmbedding = await this.embeddingsService.generateEmbedding(
+      query.searchText,
+    );
 
     // Search for related anchors in Qdrant
-    const searchResults = await this.qdrantService.searchAnchors(queryEmbedding, 100);
+    const searchResults = await this.qdrantService.searchAnchors(
+      queryEmbedding,
+      20,
+    );
 
     return new SearchRelatedAnchorsQueryResponse(searchResults);
   }
